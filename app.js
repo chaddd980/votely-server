cf5f2da82c3ee35c1875aa46cc905e1476786b8a
chadd williams <chadd980@hotmail.com>
2017-05-17 09:24:29 -0400

    edited cors in app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors())
   // res.Header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   // res.Header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   // next();
-});
+// });
 // app.use(function(req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Access-Control-Allow-Credentials', 'true');
