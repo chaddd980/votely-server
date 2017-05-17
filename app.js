e5ae642f8e3ab1e234d35545b86ab08672287a33
chadd williams <chadd980@hotmail.com>
2017-05-17 07:37:20 -0400

    adjusted cors info in app.js

--- app.js
+++ app.js
@@ -30,15 +30,20 @@ app.use(cookieParser());
 
 //To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-
-  //and remove cacheing so we get the most recent comments
-  res.setHeader('Cache-Control', 'no-cache');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//
+//   //and remove cacheing so we get the most recent comments
+//   res.setHeader('Cache-Control', 'no-cache');
+//   next();
+// });
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
